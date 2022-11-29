8a75c07f7836c9863b2b7530ab6087c3d5d68a66
bsh82 <tkdgnsqocn@gmail.com>
2022-11-30 03:30:12 +0900

    Dom을 이용하여 HTML 객체 저장

---
+++ app/src/public/js/home/index.js
@@ -0,0 +1,4 @@
+/*
+*/
+type = "text/javascript"; 
+src = "http://dapi.kakao.com/v2/maps/sdk.js?appkey=becc7422f0b5814b3b0ff2eabeb25a5b&libraries=services,clusterer,drawing";
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,13 @@
-console.log("korea");
-console.log("korea2");
+const id = document.querySelector("#id"),
+pw = document.querySelector("#pw"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+       id: id.value,
+       pw: pw.value,
+    };
+    console.log(req);
+}
